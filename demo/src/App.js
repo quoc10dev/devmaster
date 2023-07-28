6acd32ffc622e80e10ff50e48df65dc4172ec132
quoc10dev <quocnguyen.cr@gmail.com>
2023-07-28 15:20:12 +0700

    Demo React

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Logo_ags.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my First React App
         </p>
         <a
           className="App-link"
